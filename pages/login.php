eeb2a595c5d128154e5aa4c9747e10015d5f7722
Avallah <fayuthr4@gmail.com>
2023-12-03 08:46:34 -0500

    add SIGN UP to login.php

--- pages/login.php
+++ pages/login.php
@@ -1,21 +1,25 @@
 <?php
+session_start();
 $conn = mysqli_connect("localhost", "root", "", "database");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
 if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] == true) {
     header("Location: login.php");
     exit;
 }
+
 $checker = new \misc\PasswordChecker();
 if(!$checker->check($_POST["password"])) {
     echo "Funca";
 } else {
     echo "nofunca";
 }
-if (isset($_POST["username"]) && isset($_POST["password"])) {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+
+if (isset($_POST["login_username"]) && isset($_POST["login_password"])) {
+    $username = $_POST["login_username"];
+    $password = $_POST["login_password"];
     $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result) == 1) {
@@ -25,10 +29,44 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
         exit;
     }
 }
+
+if (isset($_POST["signup_username"]) && isset($_POST["signup_password"]) && isset($_POST["signup_repeat_password"])) {
+    $username = $_POST["signup_username"];
+    $password = $_POST["signup_password"];
+    $repeatPassword = $_POST["signup_repeat_password"];
+
+    if ($password !== $repeatPassword) {
+        echo "Passwords do not match.";
+    } else {
+        $checkUsernameQuery = "SELECT * FROM users WHERE username='$username'";
+        $checkUsernameResult = mysqli_query($conn, $checkUsernameQuery);
+        if (mysqli_num_rows($checkUsernameResult) > 0) {
+            echo "Username is not available.";
+        } else {
+            $insertQuery = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+            if (mysqli_query($conn, $insertQuery)) {
+                echo "Sign up successful. You can now login.";
+            } else {
+                echo "Error: " . mysqli_error($conn);
+            }
+        }
+    }
+}
+
 echo "<h1>Login</h1>";
 echo "<form action='login.php' method='post'>";
-echo "<input type='text' name='username' placeholder='Username'>";
-echo "<input type='password' name='password' placeholder='Password'>";
+echo "<input type='text' name='login_username' placeholder='Username'>";
+echo "<input type='password' name='login_password' placeholder='Password'>";
 echo "<input type='submit' value='Login'>";
 echo "</form>";
+
+echo "<h1>Sign Up</h1>";
+echo "<form action='login.php' method='post'>";
+echo "<input type='text' name='signup_username' placeholder='Username'>";
+echo "<input type='password' name='signup_password' placeholder='Password'>";
+echo "<input type='password' name='signup_repeat_password' placeholder='Repeat password'>";
+echo "<input type='submit' value='Sign Up'>";
+echo "</form>";
+
+mysqli_close($conn);
 ?>
